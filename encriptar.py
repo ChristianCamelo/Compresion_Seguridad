b3f8ede77b50be62a1cbf2964374b202683d071b
Christian Camelo Espinal <116672498+ChristianCamelo@users.noreply.github.com>
2023-10-25 11:58:51 +0200

    ULTIMA VERSION

--- encriptar.py
+++ encriptar.py
@@ -1,3 +1,4 @@
+
 import os
 from principal import escribirLog
 from Crypto.Cipher import AES
@@ -7,29 +8,52 @@ from Crypto.Random import get_random_bytes # para generar la clave y el IV
 # FUNCIÓN PARA ENCRIPTAR
 #------------------------------------------------------------------------------------------------------------------------------------
 def encriptar(fichero, cont):
-    encriptado = False
+    escribirLog("----------------INICIANDO ENCRIPTACION DE "+ fichero + "--------------")
     modo_encriptar = AES.MODE_CTR
-    iv = get_random_bytes(16)
     key = get_random_bytes(16)
-    llave_guardada = guardarKey(key, cont)
-    iv_guardadi = guardarIV(iv, cont)
-    objeto_ecriptador = AES.new(key, modo_encriptar)
-    with open(os.path.basename(fichero), 'rb') as f:
-        fichero_a_encriptar = f.read()
-        fichero_padeado = pad_fichero(fichero_a_encriptar)
-    fichero_encriptado = objeto_ecriptador.encrypt(fichero_padeado)
-    archivo_encriptado_guardado = guardar_archivo_encriptado(fichero_encriptado, cont)
-    escribirLog("prueba encriptación de un fichero en PDF")
-    if llave_guardada and archivo_encriptado_guardado:
-        encriptado = True
-    else:
-        encriptado = False
-    return encriptado
 
+    objeto_ecriptador = AES.new(key, modo_encriptar)
+    nonce = objeto_ecriptador.nonce
+    print(len(nonce),type(nonce))
+    print(key)
+    print(nonce)
+    llave_guardada = guardarKey(key, nonce, cont)
+    print(llave_guardada)
+    
+    with open(fichero, 'rb') as f:
+        fichero_a_encriptar = f.read()
+        # fichero_padeado = pad_fichero(fichero_a_encriptar)
+    fichero_encriptado = objeto_ecriptador.encrypt(fichero_a_encriptar)
+    archivo_encriptado_guardado = guardar_archivo_encriptado(fichero_encriptado, cont)
+    escribirLog("Llave generada: "+ str(llave_guardada))
+    escribirLog("----------------FINALIZANDO ENCRIPTACION DE "+ fichero + "--------------")
+    return archivo_encriptado_guardado
+#------------------------------------------------------------------------------------------------------------------------------------
+# FUNCIÓN PARA GUARDAR EL ARCHIVO ENCRIPTADO: Guarda cada archivo en la carpeta archivos_encriptados
+#------------------------------------------------------------------------------------------------------------------------------------
+# def guardar_nonce(nonce, cont):
+#     guardado = False
+#     ruta_archivos_encriptados = os.path.join(os.getcwd(), 'archivos_encriptados')
+#     ruta_archivo = os.path.join(ruta_archivos_encriptados, 'nonce' + str(cont) + '.bin')
+#     # Escribiendo el archivo
+#     with open(ruta_archivo, 'wb') as a:
+#         a.write(nonce)
+    
+#     # Comprobando que el archivo existe
+#     with open(ruta_archivo, 'rb') as b:
+#         data = b.read()
+#         if len(data) > 0:
+#             guardado = True
+#         else:
+#             guardado = False
+#     print(guardado)
+#     escribirLog("Encriptacion: Creando la ruta de guardado: "+ruta_archivo)
+#     return ruta_archivo
 #------------------------------------------------------------------------------------------------------------------------------------
 # FUNCIÓN PARA GUARDAR EL ARCHIVO ENCRIPTADO: Guarda cada archivo en la carpeta archivos_encriptados
 #------------------------------------------------------------------------------------------------------------------------------------
 def guardar_archivo_encriptado(fichero, cont):
+
     guardado = False
     ruta_archivos_encriptados = os.path.join(os.getcwd(), 'archivos_encriptados')
     ruta_archivo = os.path.join(ruta_archivos_encriptados, 'archivo' + str(cont) + '.enc')
@@ -46,13 +70,13 @@ def guardar_archivo_encriptado(fichero, cont):
         else:
             guardado = False
     print(guardado)
-    escribirLog("prueba de guardar un archivo encripado pdf")
-    return guardado
+    escribirLog("Encriptacion: Creando la ruta de guardado: "+ruta_archivo)
+    return ruta_archivo
 
 #------------------------------------------------------------------------------------------------------------------------------------
 # FUNCIÓN PARA GUARDAR LA LLAVE: Guarda cada llave en un fichero binario guardado siguiendo el formato: 'llave' + contador + '.bin'
 #------------------------------------------------------------------------------------------------------------------------------------
-def guardarKey(key, cont):
+def guardarKey(key, nonce, cont):
     success = False
     ruta_keys = os.path.join(os.getcwd(), 'keys')
     ruta_archivo = os.path.join(ruta_keys, 'llave' + str(cont) + '.bin')
@@ -60,44 +84,24 @@ def guardarKey(key, cont):
     # Escribiendo la llave
     with open(ruta_archivo, 'ab') as ficheroKeys:
         ficheroKeys.write(key)
-    
+        ficheroKeys.write(nonce)
+
     # Comprobando que el archivo existe
     with open(ruta_archivo, 'rb') as b:
         data = b.read()
-        if len(data) > 0 and len(data) <= 16:
+        print(data)
+        if len(data) > 0 and len(data) <= 24:
             success = True
+            escribirLog("Encriptacion: Se ha generado la llave : "+ruta_archivo)
         else:
             success = False
     print(success)
     return success
 
 #------------------------------------------------------------------------------------------------------------------------------------
-# FUNCIÓN PARA GUARDAR EL IV: Guarda cada iv en un fichero binario guardado siguiendo el formato: 'iv' + contador + '.bin'
-#------------------------------------------------------------------------------------------------------------------------------------
-def guardarIV(iv, cont):
-    guardado = False
-    ruta_iv = os.path.join(os.getcwd(), 'iv')
-    ruta_archivo = os.path.join(ruta_iv, 'iv' + str(cont) + 'bin')
-
-    # Escribiendo el IV
-    with open(ruta_archivo, 'ab') as fichero_iv:
-        fichero_iv.write(iv)
-
-    
-    # Comprobando que el archivo existe
-    with open(ruta_archivo, 'rb') as f:
-        data = f.read()
-        if len(data) > 0 and len(data) <= 16:
-            guardado = True
-        else:
-            guardado = False
-    print(guardado)
-    return(guardado)
-
-#------------------------------------------------------------------------------------------------------------------------------------
 # FUNCIÓN PARA PONERLE PADDING AL FICHERO
 #------------------------------------------------------------------------------------------------------------------------------------
-def pad_fichero(fichero):
-    while len(fichero) % 16 != 0:
-        fichero = fichero + b'0' # Hay que añadir bits porque es un fichero
-    return fichero
+# def pad_fichero(fichero):
+#     while len(fichero) % 16 != 0:
+#         fichero = fichero + b'0' # Hay que añadir bits porque es un fichero
+#     return fichero
--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,14 @@
 import tkinter as tk
+import os
 from tkinter import filedialog
+from encriptar import encriptar
+from principal import escribirLog
+from desencriptar import desencriptar
+from shutil import rmtree
+
+ventana = None
+global cantidad
+cantidad = 0
 
 class Archivo:
     def __init__(self, archivo):
@@ -7,52 +16,85 @@ class Archivo:
         self.cifrar = True
         self.frame = None
         self.label = None
-        self.cifrar_button = None
         self.descifrar_button = None
 
 def cargar_archivo():
+    global cantidad
     archivo = filedialog.askopenfilename()
+    ruta_keys = os.path.join(os.getcwd(), 'keys')
+    ruta_archivos_encriptados = os.path.join(os.getcwd(), 'archivos_encriptados')
+    
     if archivo:
         archivo_obj = Archivo(archivo)
+        archivo_obj.ruta = os.path.abspath(archivo)
+        archivo_obj.pos=cantidad
+        archivo_obj.enc = cifrar(archivo_obj.ruta, cantidad)
+        archivo_obj.encPath = os.path.join(archivo_obj.enc)
+        archivo_obj.format = (archivo_obj.ruta).split(".")[-1]
+        archivo_obj.keyPath = os.path.join(ruta_keys, 'llave' + str(archivo_obj.pos) + '.bin')
+        archivo_obj.noncePath = os.path.join(ruta_archivos_encriptados, 'nonce' + str(archivo_obj.pos) + '.bin')
+
+        escribirLog("Lectura: Ruta de archivo leido es: "+ archivo_obj.archivo)
+        escribirLog("Escritura: Ruta de archivo cifrado es: "+ archivo_obj.enc)
+        
+        actualizarUI()
+        crear_botones_descifrar(archivo_obj)
+        cantidad = cantidad+1
         archivos_seleccionados.append(archivo_obj)
-        crear_botones_cifrar_descifrar(archivo_obj)
 
 def toggle_botones(archivo):
-    archivo.cifrar = not archivo.cifrar
-    if archivo.cifrar:
-        archivo.cifrar_button.grid()
-        archivo.descifrar_button.grid_remove()
-    else:
-        archivo.descifrar_button.grid()
-        archivo.cifrar_button.grid_remove()
 
-def crear_botones_cifrar_descifrar(archivo_obj):
-    archivo_obj.frame = tk.Frame(ventana)
+    archivo.descifrar_button.grid()
+
+    escribirLog("Descencriptando archivo "+str(archivo.enc))
+    escribirLog("Descencriptando con llave: "+ str(archivo.keyPath))
+    escribirLog("La desencriptacion ha sido: " + str(desencriptar(archivo.encPath,archivo.keyPath,archivo.pos, archivo.format)))
+
+def cifrar(ruta,cantidad):
+    return encriptar(ruta,cantidad)
+
+def crear_botones_descifrar(archivo_obj):
+
+    archivo_obj.frame = tk.Frame(ventana, borderwidth=2, relief="solid")
     archivo_obj.frame.grid(row=len(archivos_seleccionados), column=0, padx=5, pady=5, sticky="w")
 
-    archivo_obj.label = tk.Label(archivo_obj.frame, text=archivo_obj.archivo)
-    archivo_obj.label.grid(row=0, column=0)
+    color1 = "#2233FF"
 
-    archivo_obj.cifrar_button = tk.Button(archivo_obj.frame, text="Cifrar", command=lambda a=archivo_obj: toggle_botones(a))
-    archivo_obj.descifrar_button = tk.Button(archivo_obj.frame, text="Descifrar", command=lambda a=archivo_obj: toggle_botones(a))
+    archivo_obj.label = tk.Label(archivo_obj.frame, text=archivo_obj.enc, font=("Helvetica", 12), background=color1, fg="black")
+    archivo_obj.label.grid(row=1, column=0, padx=5, pady=5)
 
-    archivo_obj.cifrar_button.grid(row=0, column=1, padx=(0, 5), sticky="e")
-    archivo_obj.descifrar_button.grid(row=0, column=1, padx=(0, 5), sticky="e")
-    archivo_obj.descifrar_button.grid_remove()
+    archivo_obj.descifrar_button = tk.Button(archivo_obj.frame, text="Descifrar", font=("Helvetica", 12, "bold"), command=lambda a=archivo_obj: toggle_botones(a))
+    archivo_obj.descifrar_button.grid(row=1, column=1, padx=(0, 5))
+
+
+def limpiar_cache():
+    # Eliminando directorios
+    rmtree("archivos_desencriptados")
+    rmtree("archivos_encriptados")
+    rmtree("keys")
+    # Creadon directorios
+    os.mkdir("archivos_desencriptados")
+    os.mkdir("archivos_encriptados")
+    os.mkdir("keys")
 
 archivos_seleccionados = []
 
-# Crear una ventana principal
-ventana = tk.Tk()
-ventana.title("Ejemplo de Cifrado y Descifrado")
+def actualizarUI():
+    # Botón para cargar un archivo
+    cargar_button = tk.Button(ventana, text="Cargar archivo", font=("Helvetica", 12, "bold"), command=cargar_archivo)
+    cargar_button.grid(row=cantidad+1, column=0, columnspan=3, pady=10)
 
-# Botón para cargar un archivo
-cargar_button = tk.Button(ventana, text="Cargar Archivo", command=cargar_archivo)
-cargar_button.grid(row=0, column=0, columnspan=3, pady=10)
+    # Botón para limpiar
+    limpiar_button = tk.Button(ventana, text="Limpiar cache", font=("Helvetica", 12, "bold"), command=limpiar_cache)
+    limpiar_button.grid(row=cantidad+2, column=0, columnspan=3, pady=10)
 
-# Etiqueta para mostrar el resultado
-resultado_label = tk.Label(ventana, text="")
-resultado_label.grid(row=1, column=0, columnspan=3, pady=10)
+def iniciar_ventana():
+    limpiar_cache()
+    ventana = tk.Tk()
+    actualizarUI()
+    ventana.title("Ejemplo de Cifrado y Descifrado")
+    escribirLog("----------------INICIANDO EL PROGRAMA--------------")
+    ventana.mainloop()
 
-# Iniciar el bucle principal de la aplicación
-ventana.mainloop()
+
+iniciar_ventana()
